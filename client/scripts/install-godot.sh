191dc7ff09f97666219fa71b7d5b644a485c4557
Bryan <bryan@datandragons.io>
2025-10-30 19:28:06 -0400

    Image overhaul plan

---
+++ client/scripts/install-godot.sh
@@ -0,0 +1,3 @@
+wget -O Godot_v4.5.1-stable_linux.x86_64.zip "https://downloads.godotengine.org/?version=4.5.1&flavor=stable&slug=linux.x86_64.zip&platform=linux.64"
+unzip Godot_v4.5.1-stable_linux.x86_64.zip
+chmod +x  Godot_v4.5.1-stable_linux.x86_64
